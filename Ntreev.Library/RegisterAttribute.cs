376194d27cb5b96eddcdb3d694f2a86654313160
엄 준일 <umc@ntreev.com>
2018-10-18 18:52:42 +0900

    RegisterAttribute 를 이용하여 IoC 컨테이너에 서비스가 등록될 수 있는 특성 추가함.

---
+++ Ntreev.Library/RegisterAttribute.cs
@@ -0,0 +1,26 @@
+﻿#if NETSTANDARD
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ntreev.Library
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+    public class RegisterAttribute : Attribute
+    {
+        public Type RegistrationType { get; }
+        public ServiceLifetime Lifetime { get; } = ServiceLifetime.Singleton;
+
+        public RegisterAttribute(Type registrationType)
+        {
+            this.RegistrationType = registrationType;
+        }
+
+        public RegisterAttribute(Type registrationType, ServiceLifetime serviceLifetime) : this(registrationType)
+        {
+            this.Lifetime = serviceLifetime;
+        }
+    }
+}
+#endif
